0537b7fee6dbb66ac7a979d65b658aed8f768a07
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-03-08 20:28:17 +0100

    Added default parameters and targets

---
+++ include/Parameters.hpp
@@ -0,0 +1,59 @@
+/* 
+PACS - First Challenge.
+Andrea Di Antonio, 10655477.
+*/
+
+#ifndef PARAMETERS_PACS
+#define PARAMETERS_PACS
+
+#include "Vector.hpp"
+
+namespace pacs {
+    
+    // Slpha strategy.
+    enum Strategy {Exponential, Inverse, Armijo};
+
+    // Default parameters.
+    struct Parameters {
+        // X_0.
+        Vector start = Vector(2, {0.0L, 0.0L});
+
+        // Alpha_0.
+        Real alpha = 0.0L;
+
+        // Tolerances.
+        Real step_tolerance = 1.0E-6L;
+        Real residual_tolerance = 1.0E-6L;
+        
+        // Maximum number of iterations.
+        size_t max_iter = 10000;
+
+        // Alpha rule.
+        Strategy strategy = Armijo;
+
+        // Strategy parameters.
+        Real strategy_mu = 0.2L; // Decays.
+        Real strategy_sigma = 0.25L; // Armijo.
+    };
+
+    // Default target function.
+    Real target(const Vector &x) {
+        assert(x.get_size() == 2);
+
+        return x[0] * x[1] + 4 * std::pow(x[0], 4) + std::pow(x[1], 2) + 3 * x[0];
+    }
+
+    // Default target gradient function.
+    Vector target_gradient(const Vector &x) {
+        assert(x.get_size() == 2);
+
+        Vector gradient = Vector(2);
+
+        gradient[0] = x[1] + 16 * std::pow(x[0], 3) + 3;
+        gradient[1] = x[0] + (2 * x[1]);
+
+        return gradient;
+    }
+}
+
+#endif
